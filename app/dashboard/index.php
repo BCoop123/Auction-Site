2d1d51e8eb78cd18b4694017d89188d04a8e8071
Ben <btmolloy2@gmail.com>
2023-10-15 21:25:59 -0400

    Nav nav

--- app/dashboard/index.php
+++ app/dashboard/index.php
@@ -10,8 +10,7 @@ importHeader($pathToSurface);
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <h1 class="display-4 fw-bolder">Traditional Auction</h1>
                 </div>
             </div>
         </header>
@@ -221,6 +220,6 @@ importHeader($pathToSurface);
         <!-- Footer-->
         <?php
         importFooter($pathToSurface);
-        ?> 
+        ?>
     </body>
 </html>
--- lib/multipageFunctions.php
+++ lib/multipageFunctions.php
@@ -22,14 +22,35 @@ function getProfileImagePath($pathToSurface, $defaultImg = '/data/assets/account
     return $pathToSurface . $defaultImg;
 }
 
-function populateSignAction(){
+function getProfileLink($pathToSurface){
     if (isset($_COOKIE["username"])) {
+        
+        return $pathToSurface . "/app/profile/index.php";
 
+    
+    }else {
+        return $pathToSurface . "/app/login/login.php";
+ 
+    }
 
+}
+
+function populateSignActionNav($pathToSurface){
+    if (isset($_COOKIE["username"])) {
+        return '<form action="' . $pathToSurface . '/lib/logout.php" method="post" style="margin: 0; padding: 0;"><button type="submit" class="custom-btn sign-out-button">Sign Out</button></form>';    
     }
     else {
+        return '<a href="' . $pathToSurface . '/app/login/login.php"><div class="custom-btn">Sign In / Sign Up</div></a>';
 
     }
 }
+function goToAccount($pathToSurface){
+    if (isset($_COOKIE["username"])) {
+        return '<a href="' . $pathToSurface . '/app/profile/index.php"><div class="custom-btn">Account Details</div></a>';
+    }
+    else {
+        return '<a href="' . $pathToSurface . '/app/login/login.php"><div class="custom-btn">Account Details</div></a>';
 
+    }
+}
 ?>
