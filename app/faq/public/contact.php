714af829447182d405eef9a1822392adb1ef3b71
LogaPoe <loganpoe430@gmail.com>
2023-12-11 13:32:43 -0500

    Created class instance for user and contact

--- app/faq/public/contact.php
+++ app/faq/public/contact.php
@@ -1,16 +1,31 @@
 <?php
-require_once("../../../lib/multipageFunctions.php");
-require_once("../../../themes/components/header_footer_import.php");
-$pathToSurface = "../../..";
 
-importHeader($pathToSurface);
-?>
+class ContactForm
+{
+    private $pathToSurface;
 
-    <div class="container">
-        <h1 class="mt-5">Contact Us</h1>
+    public function __construct($pathToSurface)
+    {
+        $this->pathToSurface = $pathToSurface;
+    }
 
-        <?php
+    public function importHeader()
+    {
+        require_once("{$this->pathToSurface}/lib/multipageFunctions.php");
+        require_once("{$this->pathToSurface}/themes/components/header_footer_import.php");
+
+        importHeader($this->pathToSurface);
+    }
+
+    public function importFooter()
+    {
+        importFooter($this->pathToSurface);
+    }
+
+    public function processForm()
+    {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Form data processing logic
             $name = $_POST["name"];
             $email = $_POST["email"];
             $message = $_POST["message"];
@@ -23,7 +38,7 @@ importHeader($pathToSurface);
             );
 
             // Load existing data from contact.json
-            $existingData = json_decode(file_get_contents("../../../data/contact/contact.json"), true);
+            $existingData = json_decode(file_get_contents("{$this->pathToSurface}/data/contact/contact.json"), true);
 
             // Add the new form data to the existing data
             $existingData[] = $formData;
@@ -32,40 +47,56 @@ importHeader($pathToSurface);
             $jsonData = json_encode($existingData, JSON_PRETTY_PRINT);
 
             // Save the JSON data back to contact.json
-            file_put_contents("../../../data/contact/contact.json", $jsonData);
+            file_put_contents("{$this->pathToSurface}/data/contact/contact.json", $jsonData);
 
             // Display a success message
             echo "<div class='alert alert-success mt-3'>Thank you for your submission!</div>";
         }
+    }
+
+    public function renderForm()
+    {
         ?>
+        <div class="container">
+            <h1 class="mt-5">Contact Us</h1>
 
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" class="form-control" name="name" id="name" required>
-            </div>
+            <!-- Form rendering logic -->
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" name="name" id="name" required>
+                </div>
 
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" class="form-control" name="email" id="email" required>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
 
-            <div class="form-group">
-                <label for="message">Message:</label>
-                <textarea class="form-control" name="message" id="message" rows="4" required></textarea>
-            </div>
+                <div class="form-group">
+                    <label for="message">Message:</label>
+                    <textarea class="form-control" name="message" id="message" rows="4" required></textarea>
+                </div>
 
-            <button type="submit" class="btn btn-primary" style="margin-top: 20px">Submit</button>
-        </form>
+                <button type="submit" class="btn btn-primary" style="margin-top: 20px">Submit</button>
+            </form>
 
-        <!-- Back button to go back a page -->
-        <a href="javascript:history.back()" class="btn btn-secondary mt-3" style="margin-bottom: 20px">Back</a>
-    </div>
+            <!-- Back button to go back a page -->
+            <a href="javascript:history.back()" class="btn btn-secondary mt-3" style="margin-bottom: 20px">Back</a>
+        </div>
+        <?php
+    }
+}
 
-<!-- Footer-->
-<?php
-importFooter($pathToSurface);
-?> 
+// Create an instance of the ContactForm class
+$contactForm = new ContactForm("../../..");
+
+// Call methods to import header, process form, and render form
+$contactForm->importHeader();
+$contactForm->processForm();
+$contactForm->renderForm();
+$contactForm->importFooter();
+?>
+
 
 
