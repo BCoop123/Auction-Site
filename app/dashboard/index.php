f835930c6e18ac19db8b8d20de20f1c3524c4a34
BenjaminAcuff <bacuff04@yahoo.com>
2023-12-15 20:28:19 -0500

    Update index.php

--- app/dashboard/index.php
+++ app/dashboard/index.php
@@ -1,32 +1,14 @@
 <?php
 require_once("../../lib/multipageFunctions.php");
 require_once("../../themes/components/header_footer_import.php");
-
-// Include database connection and path config
 require_once('../../lib/settings.php');
-
-// Logic that gets the relative path and root directory
-$currentScriptDirectory = dirname(__FILE__); // or __DIR__ in PHP 5.3 and later
-$rootDirectory = getRootDirectory();
-$relativePathToRoot = getRelativePathToRoot($currentScriptDirectory, $rootDirectory);
-
-importHeader($relativePathToRoot);
-
-// SQL query to retrieve auction information with image details
-$query = '
-    SELECT a.auction_id, a.title, a.highest_bid, i.image_name
-    FROM auction AS a
-    JOIN auctionimagerelationship AS air ON a.auction_id = air.auction_id
-    JOIN image AS i ON air.image_id = i.image_id
-';
-$statement = $pdo->query($query);
 
 // Function to display auction information in the desired format
 function display_auction_info($auction) {
     global $relativePathToRoot;
     echo '<div class="col mb-5">';
     echo '<div class="card h-100">';
-    echo '<img class="card-img-top" src="' . $relativePathToRoot . '/path/to/your/image/folder/' . $auction['image_name'] . '" alt="Auction Image" />';
+    echo '<img class="card-img-top" src="' . $relativePathToRoot . '/path/to/placeholder/image.jpg" alt="Auction Image" />'; // Use a placeholder image
     echo '<div class="card-body p-4">';
     echo '<div class="text-center">';
     echo '<h5 class="fw-bolder">' . $auction['title'] . '</h5>';
@@ -40,7 +22,102 @@ function display_auction_info($auction) {
     echo '</div>';
 }
 
+$currentScriptDirectory = dirname(__FILE__);
+$rootDirectory = getRootDirectory();
+$relativePathToRoot = getRelativePathToRoot($currentScriptDirectory, $rootDirectory);
+
+importHeader($relativePathToRoot);
+
+// Function to save auction data to the database
+function saveAuctionToDatabase($pdo, $title, $description, $startingBid, $startDate, $endDate) {
+    try {
+        // Start a transaction to ensure atomicity
+        $pdo->beginTransaction();
+
+        // Prepare the SQL statement for auction insertion
+        $stmtAuction = $pdo->prepare("INSERT INTO auction (title, description, starting_bid, highest_bid, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)");
+
+        // Set highest_bid equal to starting_bid
+        $highestBid = $startingBid;
+
+        // Bind parameters for auction insertion
+        $stmtAuction->bindParam(1, $title);
+        $stmtAuction->bindParam(2, $description);
+        $stmtAuction->bindParam(3, $startingBid);
+        $stmtAuction->bindParam(4, $highestBid);
+        $stmtAuction->bindParam(5, $startDate);
+        $stmtAuction->bindParam(6, $endDate);
+
+        // Execute the auction insertion statement
+        $stmtAuction->execute();
+
+        // Get the last inserted auction_id
+        $auctionId = $pdo->lastInsertId();
+
+        // Prepare the SQL statement for auctionimagerelationship insertion
+        $stmtAir = $pdo->prepare("INSERT INTO auctionimagerelationship (auction_id, image_id) VALUES (?, ?)");
+
+        // Use a placeholder image_id for now; replace it with the actual image_id
+        $imageId = 1;
+
+        // Bind parameters for auctionimagerelationship insertion
+        $stmtAir->bindParam(1, $auctionId);
+        $stmtAir->bindParam(2, $imageId);
+
+        // Execute the auctionimagerelationship insertion statement
+        $stmtAir->execute();
+
+        // Prepare the SQL statement for image insertion
+        $stmtImage = $pdo->prepare("INSERT INTO image (image_name) VALUES (NULL)");
+
+        // Execute the image insertion statement
+        $stmtImage->execute();
+
+        // Commit the transaction
+        $pdo->commit();
+    } catch (PDOException $e) {
+        // Rollback the transaction on error
+        $pdo->rollBack();
+
+        // Handle any errors here
+        echo "<div class='alert alert-danger mt-3'>Error saving auction to database: " . $e->getMessage() . "</div>";
+    }
+}
+
+// Fetch auction data from the database
+try {
+    // SQL query to retrieve auction information with image details
+    $query = '
+        SELECT a.auction_id, a.title, a.highest_bid, i.image_name
+        FROM auction AS a
+        JOIN auctionimagerelationship AS air ON a.auction_id = air.auction_id
+        JOIN image AS i ON air.image_id = i.image_id
+    ';
+    $stmt = $pdo->query($query);
+} catch (PDOException $e) {
+    // Handle query execution error
+    echo "<div class='alert alert-danger mt-3'>Error executing query: " . $e->getMessage() . "</div>";
+}
+
+// Form submission logic
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $startingBid = $_POST["startingBid"];
+    $startDate = $_POST["startDate"];
+    $endDate = $_POST["endDate"];
+
+    // Debug: Output form data
+    echo "Title: $title, Description: $description, Starting Bid: $startingBid, Start Date: $startDate, End Date: $endDate";
+
+    // Save form data to the database
+    saveAuctionToDatabase($pdo, $title, $description, $startingBid, $startDate, $endDate);
+
+    // Display a success message
+    echo "<div class='alert alert-success mt-3'>Auction created successfully!</div>";
+}
 ?>
+
 <!-- Header-->
 <header class="bg-dark py-5">
     <div class="container px-4 px-lg-5 my-5">
@@ -49,22 +126,58 @@ function display_auction_info($auction) {
         </div>
     </div>
 </header>
+
 <!-- Section-->
-<div class="container"> <!-- Adding a container for the grid system -->
-    <div class="row justify-content-center"> <!-- This row centers the col-10 content -->
+<div class="container">
+    <div class="row justify-content-center">
         <div class="col-11">
             <section class="py-5">
-                <?php
-                echo '<div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">';
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                    display_auction_info($row);
-                }
-                echo '</div>';
-                ?>
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <?php
+                    // Debug: Output each row
+                    while ($row = $stmt->fetch()) {
+                        display_auction_info($row);
+                    }
+                    ?>
+                </div>
             </section>
+
+            <!-- Create Auction Form -->
+            <div class="mt-5">
+                <h2>Create Auction</h2>
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                    <div class="form-group">
+                        <label for="title">Title:</label>
+                        <input type="text" class="form-control" name="title" id="title" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">Description:</label>
+                        <textarea class="form-control" name="description" id="description" rows="4" required></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="startingBid">Starting Bid:</label>
+                        <input type="number" class="form-control" name="startingBid" id="startingBid" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="startDate">Start Date:</label>
+                        <input type="datetime-local" class="form-control" name="startDate" id="startDate" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="endDate">End Date:</label>
+                        <input type="datetime-local" class="form-control" name="endDate" id="endDate" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" style="margin-top: 20px">Create Auction</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+
 <!-- Footer-->
 <?php
 importFooter($relativePathToRoot);
